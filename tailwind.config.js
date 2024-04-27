c2849226933443083dd9e9896c1f6e46afd6a44d
Sandipan Banik <sandipanbanik2@gmail.com>
2024-04-27 22:18:00 +0530

    first step done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["/*/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontsize:{
+        sm:"0.75rem",
+        base:"0.875rem",
+        md:"1rem",
+        lg:"1.25rem",
+        xl:"1.5rem",
+        "2xl":"1.75rem",
+        "3xl":"2.25rem",
+      }
+    },
   },
   plugins: [],
 }
